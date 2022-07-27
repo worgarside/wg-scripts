c406eb09a77988f63fcb9b6b512fed9ad69b6b39
Will Garside <worgarside@gmail.com>
2022-07-27 01:00:22 +0100

    Apply pre-commit hooks to entire project

--- macbook/hass_webhooks.py
+++ macbook/hass_webhooks.py
@@ -1,16 +1,16 @@
 """Home Assistant webhooks"""
 from os import getenv
 
-from requests import post
 from dotenv import load_dotenv
 from psutil import sensors_battery
+from requests import post
 
 load_dotenv()
 
 HASS_BASE_URL = getenv("HASS_BASE_URL")
 
 
-def update_battery_percentage_sensor():
+def update_battery_percentage_sensor() -> None:
     """Send MacBook battery info to Home Assistant"""
     res = post(
         f"{HASS_BASE_URL}/api/webhook/will_s_macbook_pro_battery_percentage",
