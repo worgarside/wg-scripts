112a3f6c7932890ca1311e9a2e766c5a9a723552
Will Garside <worgarside@gmail.com>
2022-07-28 22:31:20 +0100

    Avoid SSL error logs

--- macbook/hass_webhooks.py
+++ macbook/hass_webhooks.py
@@ -4,6 +4,7 @@ from os import getenv
 from dotenv import load_dotenv
 from psutil import sensors_battery
 from requests import post
+from requests.exceptions import SSLError
 from wg_utilities.exceptions import on_exception  # pylint: disable=no-name-in-module
 
 load_dotenv()
@@ -14,17 +15,19 @@ HASS_BASE_URL = getenv("HASS_BASE_URL")
 @on_exception()  # type: ignore[misc]
 def update_battery_percentage_sensor() -> None:
     """Send MacBook battery info to Home Assistant"""
-    res = post(
-        f"{HASS_BASE_URL}/api/webhook/will_s_macbook_pro_battery_percentage",
-        json={
-            "battery_percentage": sensors_battery().percent,
-            "battery_seconds_left": sensors_battery().secsleft,
-            "power_plugged": sensors_battery().power_plugged,
-        },
-        headers={"Content-Type": "application/json"},
-    )
-
-    print(res.status_code, res.reason)
+    try:
+        res = post(
+            f"{HASS_BASE_URL}/api/webhook/will_s_macbook_pro_battery_percentage",
+            json={
+                "battery_percentage": sensors_battery().percent,
+                "battery_seconds_left": sensors_battery().secsleft,
+                "power_plugged": sensors_battery().power_plugged,
+            },
+            headers={"Content-Type": "application/json"},
+        )
+        print(res.status_code, res.reason)
+    except SSLError:
+        pass
 
 
 if __name__ == "__main__":
